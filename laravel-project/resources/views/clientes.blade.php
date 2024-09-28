a2add2f3fdccba014bed977c0af3ea17f34fb2ab
Oscar Muñoz <dealo2729@gmail.com>
2024-09-28 14:04:37 -0600

    Creación de 2 páginas
    Se crearon dos páginas nuevas: formulario y clientes. En el formulario es donde se registrarán los clientes y en la página de Clientes es donde se podrán consultar los clientes ya registrados

---
+++ laravel-project/resources/views/clientes.blade.php
@@ -0,0 +1,37 @@
+
+  @extends('layouts.plantillas')
+  @section('titulo', 'Clientes')
+  @section('contenido')
+  
+  
+
+    {{-- Inicia tarjetaCliente --}}
+    <div class="container mt-5 col-md-8">
+
+    <div class="card text-justify font-monospace">
+
+        <div class="card-header fs-5 text-primary">
+            Óscar Miguel Muñoz Arroyo
+
+        </div>
+
+        <div class="card-body">
+            <h5 class="fw-blod">oscar.munoz@gmail.com</h5>
+            <h5 class="fw-medium">4421390537</h5>
+            <p class="card-text fw-lighter"> </p>
+
+        </div>
+
+        <div class="card-footer text-muted">
+            <button type="submit" class="btn btn-warning btn-sm">Actualizar</button>
+            <button type="submit" class="btn btn-danger btn-sm">Eliminar</button>
+
+        </div>
+
+    </div>
+    {{-- Fin tarjetaCliente --}}
+
+
+    </div>
+
+    @endsection
