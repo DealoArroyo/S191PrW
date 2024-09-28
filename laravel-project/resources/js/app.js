23349ae75b089a057f7e79bea7dba684acdb18ed
Oscar Muñoz <dealo2729@gmail.com>
2024-09-28 14:01:57 -0600

    Cambio de página de inicio
    Se eliminó el archivo "welcome" y se creó una página nueva de inicio. También se modificaron las rutas de la página

--- laravel-project/resources/js/app.js
+++ laravel-project/resources/js/app.js
@@ -1,5 +1,6 @@
 import './bootstrap';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
 import Alpine from 'alpinejs';
 
 window.Alpine = Alpine;
--- laravel-project/routes/web.php
+++ laravel-project/routes/web.php
@@ -1,20 +1,10 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view(uri: '/', view: 'inicio');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::view(uri: '/formulario', view: 'formulario')->name('cico');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+Route::view(uri: '/consultar', view: 'clientes')->name('osito');
 
-require __DIR__.'/auth.php';
